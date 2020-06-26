c66032b8e5e6a7b5c1f6367d3eb756965466b241
Eric de Graaf <endegraaf@gmail.com>
2020-06-27 00:08:50 +0200

    initial commit

---
+++ get-arch-bootstrap.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+ARCH_VERSION=2020.06.01
+LVM_PARTITION=rhel-arch--root
+MOUNT_POINT=/mnt/archroot
+SCRIPT_DIR=$HOME/tmp
+
+export PACSTRAP=false
+export BOOTSTRAP_PREP=false
+
+echo "Mount ${LVM_PARTITION} to mount point ${MOUNT_POINT}"
+if [[ ! -d /mnt/archroot ]];
+    echo "The dir ${MOUNT_POINT} does not exist, create it. "
+    then sudo mkdir /mnt/archroot
+fi
+sudo mount /dev/mapper/$LVM_PARTITION $MOUNT_POINT
+
+mkdir $SCRIPT_DIR && cd $SCRIPT_DIR
+
+if ($PACSTRAP); then
+    # https://wiki.archlinux.org/index.php/Install_Arch_Linux_from_existing_Linux
+    echo "Install Fedora pre-requisites (pacman and arch install scripts)"
+    sudo dnf install -y pacman arch-install-scripts
+    sudo pacman-key --init
+    sudo pacman-key --populate archlinux
+    sudo pacstrap /mnt/archroot base linux-lts linux-lts-headers vim lvm2 linux-firmware dialog iw dhcpcd networkmanager sudo emacs firefox steam neofetch obs-studio chromium git tinyxml2 kdiff3 remmina freerdp krita gimp fuse2 fuse3 libreoffice-fresh-nl mc man-pages man terminus-font terminus-font-otb rxvt-unicode rxvt-unicode-terminfo virtualbox vagrant virtualbox-sdk i3 dmenu rofi clipmenu perl-anyevent-i3 copyq python-pytest-runner python-mock  python-cached-property python-docopt python-texttable python-websocket-client python-docker python-dockerpty python-jsonschema python-paramiko npm maven 
+fi
+
+if ($BOOTSTRAP_PREP); then
+    echo "get the bootstrap and chroot into there (${SCRIPT_DIR}/root.x86_64/) "
+
+    if [[ ! -f archlinux-bootstrap-$ARCH_VERSION-x86_64.tar.gz.sig ]]; then 
+        echo "download sig file"
+        wget http://nlrtm1-edge1.cdn.i3d.net/o1/k9999/pub/archlinux/iso/$ARCH_VERSION/archlinux-bootstrap-$ARCH_VERSION-x86_64.tar.gz.sig
+    fi
+    if [[ ! -f archlinux-bootstrap-$ARCH_VERSION-x86_64.tar.gz ]]; then
+        echo "download bootstrap tar.gz"
+        wget http://nlrtm1-edge1.cdn.i3d.net/o1/k9999/pub/archlinux/iso/$ARCH_VERSION/archlinux-bootstrap-$ARCH_VERSION-x86_64.tar.gz
+    fi
+    gpg --keyserver-options auto-key-retrieve --verify archlinux-bootstrap-$ARCH_VERSION-x86_64.tar.gz.sig
+    echo "extract the archive"
+    sudo tar xzf archlinux-bootstrap-$ARCH_VERSION-x86_64.tar.gz 
+fi
+
+echo "arch-chroot into ${MOUNT_POINT}"
+sudo grub2-editenv - set menu_show_once=1
+curl -o '' >  ./root.x86_64/opt/aftercare.sh 
+sudo ./root.x86_64/bin/arch-chroot /mnt/archroot/
+
+# From the chroot environment adjust the Kernel Mode Settings
+# Add the required drivers there, in my case:
+# intel drivers / lvm2 
+# vim /etc/mkinitcpio.conf 
+# add i915 for intel graphics and add lvm2 in the hooks
+
+#sudo vim /etc/grub.d/40_custom
+#menuentry 'Arch Linux' {
+#    linuxefi (lvm/rhel-arch--root)/boot/vmlinuz-linux-lts root=/dev/mapper/rhel-arch--root rd.lvm.lv=rhel/rhel-arch--root
+#    initrdefi (lvm/rhel-arch--root)/boot/initramfs-lts-linux.img
+#}
+
+# rootpassword / addusers
+# from Fedora 
+# /fstab 
+
+# yay 
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -si
+

c62652ed3b601fef26a4a8e71ce15dddc4fd980e
Eric de Graaf <endegraaf@gmail.com>
2020-06-27 16:46:08 +0200

    Add packages here

--- arch-postinstall.sh
+++ arch-postinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Aftercare script"
-
-
+pacman -Sy
+pacman -S --noconfirm dhclient vim lvm2 linux-firmware dialog iw dhcpcd networkmanager sudo emacs firefox neofetch obs-studio chromium git tinyxml2 kdiff3 remmina freerdp krita gimp fuse2 fuse3 libreoffice-fresh-nl mc man-pages man terminus-font terminus-font-otb rxvt-unicode rxvt-unicode-terminfo virtualbox vagrant virtualbox-sdk i3 dmenu rofi clipmenu perl-anyevent-i3 copyq python-pytest-runner python-mock  python-cached-property python-docopt python-texttable python-websocket-client python-docker python-dockerpty python-jsonschema python-paramiko npm maven 

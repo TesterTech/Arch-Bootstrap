6749e638cd870b1607ec35a79448226e5fd13584
Eric de Graaf <endegraaf@gmail.com>
2022-04-17 23:33:37 +0200

    Update get-arch-bootstrap.sh

--- get-arch-bootstrap.sh
+++ get-arch-bootstrap.sh
@@ -11,7 +11,7 @@ install -m 755 arch-bootstrap.sh /usr/local/bin/arch-bootstrap
 echo "Note:"
 echo "Automatic fstab in /etc/fstab make sure to change to real config"
 cd /tmp
-sudo genfstab  -U / > /tmp/fstab
+sudo genfstab  -U / >> /tmp/fstab
 sudo cp -f /tmp/fstab $MOUNT_POINT/tmp/fstab
 
 echo "Download the postinstall and copy to ${MOUNT_POINT}"

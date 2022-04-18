8655e31746aaf28688caa6f3568d96742a82b28c
Eric de Graaf <endegraaf@gmail.com>
2022-04-18 22:48:17 +0200

    Update get-arch-bootstrap.sh

--- get-arch-bootstrap.sh
+++ get-arch-bootstrap.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-PARTITION=/dev/sda6 # <-- ! change this to your own partition !
+PARTITION=/dev/sda4 # <-- ! change this to your own partition !
 MOUNT_POINT=/opt
 sudo mount $PARTITION $MOUNT_POINT
 
 sudo apt install -y git vim openssh-server curl zstd arch-install-scripts
-wget https://raw.githubusercontent.com/TesterTech/arch-bootstrap-from-ubuntu/master/arch-bootstrap.sh
-wget https://raw.githubusercontent.com/TesterTech/arch-bootstrap-from-ubuntu/master/get-pacman-dependencies.sh
+curl https://raw.githubusercontent.com/TesterTech/arch-bootstrap-from-ubuntu/master/arch-bootstrap.sh -o arch-bootstrap.sh
+curl https://raw.githubusercontent.com/TesterTech/arch-bootstrap-from-ubuntu/master/get-pacman-dependencies.sh -o get-pacman-dependencies.sh
 install -m 755 arch-bootstrap.sh /usr/local/bin/arch-bootstrap
 
 echo "Note:"
 echo "Automatic fstab in /etc/fstab make sure to change to real config"
 cd /tmp
 sudo genfstab  -U / >> /tmp/fstab
+sudo mkdir -p $MOUNT_POINT/tmp
 sudo cp -f /tmp/fstab $MOUNT_POINT/tmp/fstab
 
 echo "Download the postinstall and copy to ${MOUNT_POINT}"
